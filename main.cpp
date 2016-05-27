0d5d93249617cd5e65f6afbd9cbf94027da0ced1
Damian Stolarczyk <damianstolar95@gmail.com>
2016-05-27 13:52:11 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Damian Stolarczyk
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Warcaby
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
